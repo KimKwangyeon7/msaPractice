f63e4f3b1bbfa555076d7a0e8134a04f9d80aef7
kwang9705 <kwang9705@gmail.com>
2025-01-14 19:36:51 +0900

    [BE] feat: docker setting

--- auth-service/src/main/java/com/msa/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/msa/auth_service/AuthServiceApplication.java
@@ -2,11 +2,12 @@ package com.msa.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class AuthServiceApplication {
+public class AuthServiceApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
